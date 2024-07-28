a0a41a58b98b4ffcd0e4696e7ca964b4736b4f8c
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-07-28 14:25:18 -0700

    Changes Done

--- function.js
+++ function.js
@@ -4,11 +4,11 @@ window.addEventListener('scroll',()=>{
     let Scrolltop=document.getElementsByClassName('scroll-top')[0];
     if(scroll>=200){
         Navbar.classList.add('bg-nav-scroll')
-        Scrolltop.classList.add('d-block')
+        Scrolltop.classList.add('d-scroll-btn')
       }
       else{
         Navbar.classList.remove('bg-nav-scroll')
-        Scrolltop.classList.remove('d-block')
+        Scrolltop.classList.remove('d-scroll-btn')
     }
 })
   document.getElementById('nav-profile-design').addEventListener('click',()=>{
